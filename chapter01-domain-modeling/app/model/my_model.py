7be41d5ea56e225fabfce781f0a68d972457875d
Julian <31945585+jlopezscala@users.noreply.github.com>
2021-08-31 23:22:05 -0300

    refactor(Batch): Keep order lines (#1)

--- chapter01-domain-modeling/app/model/my_model.py
+++ chapter01-domain-modeling/app/model/my_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Set
 
 
 @dataclass(frozen=True)
@@ -11,14 +12,28 @@ class OrderLine:
 
 class Batch:
 
-    def __init__(self, batch_id: str, sku: str, eta: datetime, available_quantity: int):
+    def __init__(self, batch_id: str, sku: str, eta: datetime, quantity: int):
         self._batch_id = batch_id,
         self.sku = sku
         self.eta = eta
-        self.available_quantity = available_quantity
+        self._purchased_quantity = quantity
+        self._allocations: Set[OrderLine] = set()
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
 
     def allocate(self, order_line: OrderLine):
-        self.available_quantity -= order_line.quantity
+        if self.can_allocate():
+            self._allocations.add(order_line)
 
-    def can_allocate(self, order_line: OrderLine):
+    def can_allocate(self, order_line: OrderLine) -> bool:
         return order_line.sku == self.sku and order_line.quantity <= self.available_quantity
+
+    def deallocate(self, order_line: OrderLine):
+        if order_line in self._allocations:
+            self._allocations.remove(order_line)
