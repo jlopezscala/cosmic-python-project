ec048530e532e988d7a40cc55ab1b83889669802
Julian <31945585+jlopezscala@users.noreply.github.com>
2021-09-16 10:54:06 -0300

    chore: Add Allocate function in the model (#4)

--- chapter01-domain-modeling/app/model/model.py
+++ chapter01-domain-modeling/app/model/model.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Set
+from typing import Set, List
+
+
+class OutOfStockError(Exception):
+    pass
 
 
 @dataclass(frozen=True)
@@ -12,8 +16,8 @@ class OrderLine:
 
 class Batch:
 
-    def __init__(self, batch_id: str, sku: str, eta: datetime, quantity: int):
-        self._batch_id = batch_id,
+    def __init__(self, reference: str, sku: str, eta: datetime, quantity: int):
+        self.reference = reference
         self.sku = sku
         self.eta = eta
         self._purchased_quantity = quantity
@@ -37,3 +41,21 @@ class Batch:
     def deallocate(self, order_line: OrderLine):
         if order_line in self._allocations:
             self._allocations.remove(order_line)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: List[Batch]):
+    try:
+        batch = next(
+            batch for batch in sorted(batches) if batch.can_allocate(line)
+        )
+    except StopIteration:
+        raise OutOfStockError(f"Out of stock for SKU {line.sku}")
+    batch.allocate(line)
+    return batch.reference
